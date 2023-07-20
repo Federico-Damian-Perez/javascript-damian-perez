f55742d5cdca15c5e46e990a35b3042aade6796b
Federico Perez <damipe90@gmail.com>
2023-07-20 15:41:12 -0300

    entrega 2 carrito

--- javascript.js
+++ javascript.js
@@ -1,35 +1,97 @@
-// EJERCICIO DESCUENTO DE PRODUCTOS
+// LISTADO DE PRODUCTOS
 
-let precio1 = ""
-let precio2 = ""
-let precioFinal = 0
-let nuevoPrecio=true;
-
-do{
-    precio1 = Number(prompt("Ingrese el precio original del producto"))
-    precio2 = Number(prompt("Ingrese el precio final del producto"))
-
-
-    if(precio2 > precio1){
-
-        precioFinal = ((precio2/precio1)*100)
-        precioFinal = precioFinal.toFixed(2)
+let productos = [
+    {
+      id: 1,
+      nombreProducto: "Tazon termico",
+      precio: 2500,
+    },
+    {
+      id: 2,
+      nombreProducto: "Duo de tazas termicas",
+      precio: 2800,
+    },
+    {
+      id: 3,
+      nombreProducto: "Mate termico",
+      precio: 2300,
+    },
+    {
+      id: 4,
+      nombreProducto: "Vaso grande de porcelana fina",
+      precio: 2450,
+    },
+    {
+      id: 5,
+      nombreProducto: "Vaso chico de porcelana fina",
+      precio: 1200,
+    },
     
-        alert ("Se realizo una recarga del " + precioFinal + "%")
-        document.write(`El precio final (${precio2}) tiene un ${precioFinal}% de recarga sobre el valor de ${precio1}<br>`)
-    }else {
-        
-        precioFinal = ((precio2/precio1)*100)
-        precioFinal = precioFinal.toFixed(2)
+  ];
+  
+  // BUSCAR UN PRODUCTO
 
-        alert("Se realizo un descuento del " + precioFinal + "%")
-        document.write(`El precio final (${precio1}) tiene un ${precioFinal}% de descuento sobre el valor de ${precio2}<br>`)
+  let carrito = [];
+  
+  function buscarProducto() {
+    let seleccion = prompt("Ingrese el nombre del producto que desea seleccionar: (Tazon termico, Duo de tazas termicas, Mate termico, Vaso grande de porcelana fina, Vaso chico de porcelana fina)");
+    
+    producto = productos.find((p) => p.nombreProducto.toLowerCase() === seleccion.toLowerCase());
+  }
 
+// AGREGAR PRODUCTOS A CARRITO
+  
+  function agregarCarrito() {
+    if (producto) {
+      let cantidad = parseInt(prompt("Ingrese la cantidad que desea seleccionar:"));
+      carrito.push({
+        producto: producto.nombreProducto,
+        cantidad: cantidad,
+        subtotal: producto.precio * cantidad
+      });
+    } else {
+      alert("El producto seleccionado no existe. Por favor, vuelva a intentarlo.");
     }
+  }
+  
+  // CONFIRMACION DE CARRITO
 
-    if(nuevoPrecio){
-        nuevoPrecio = confirm("Desea realizar un nuevo calculo?")
-    } 
+  function confirmarCarrito() {
+    while (true) {
+      buscarProducto();
+      agregarCarrito();
+  
+      if (!confirm("¿Desea agregar otro producto al carrito?")) {
+        break;
+      }
+    }
+  }
 
-}while(nuevoPrecio)
+  // BUSCAMOS EL CALCULO TOTAL
+  
+  function calcularTotal() {
+    console.log("Carrito de compras:");
+    carrito.forEach((item) => {
+      console.log(`- ${item.cantidad} ${item.producto}: ${item.subtotal}`);
+    });
+  
+    let total = carrito.reduce((sum, item) => sum + item.subtotal, 0);
+    console.log(`Total a pagar: ${total}`);
+  }
 
+  // VACIAR EL CARRITO
+  
+  function vaciarCarrito() {
+    carrito = [];
+    console.log("El carrito ha sido vaciado.");
+  }
+  
+  confirmarCarrito();
+  
+  if (carrito.length > 0) {
+    if (confirm("¿Desea vaciar el carrito?")) {
+      vaciarCarrito();
+    }
+  }
+  
+  calcularTotal();
